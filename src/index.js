367f3b47dc1eef34fe8c3f5e6ed5fb9fd2c2ddb9
pernilsalat <jazzel88@gmail.com>
2021-05-05 14:59:53 +0200

    library dependency bugfix

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { createStore } from "./createStore";
+export { xstate } from "./middlewares/xstate";
+export { redux } from "./middlewares/redux";
+export { devtools } from "./middlewares/devtools";
+export { persist } from "./middlewares/persist";
